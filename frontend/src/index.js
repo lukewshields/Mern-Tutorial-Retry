9c78b6906527b860688f8a26d2a78988b2d5d972
lukewshields <lukews@uw.edu>
2024-01-18 19:11:22 -0800

    Removed react-dev-tools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { WorkoutContextProvider } from './context/WorkoutContext';
 import { AuthContextProvider } from './context/AuthContext';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
